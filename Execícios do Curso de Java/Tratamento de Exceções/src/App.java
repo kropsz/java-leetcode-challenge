cb81c44cd9be6f738d5087185f702d5f6614f402
Pedro Esteves <kropinho211@outlook.com>
2023-12-21 11:10:48 -0300

    Atividade Tratamento de Exceções

--- Execícios do Curso de Java/Tratamento de Exceções/src/App.java
+++ Execícios do Curso de Java/Tratamento de Exceções/src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+import entities.Account;
+import exceptions.BusinessException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe os dados da conta");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial Balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw Limit: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.printf("New Balance: %.2f%n", acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
+
     }
 }
